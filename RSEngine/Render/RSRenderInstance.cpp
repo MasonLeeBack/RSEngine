cdc6d88e16154a8d543eabbb2c5fe9b0565ff840
Mason Lee Back <masonleeback@gmail.com>
2019-04-23 01:01:56 -0400

    Render: Some optimizations

--- RSEngine/Render/RSRenderInstance.cpp
+++ RSEngine/Render/RSRenderInstance.cpp
@@ -35,62 +35,55 @@ namespace rs {
                 std::shared_ptr<Part> p = std::static_pointer_cast<Part>(kid);
                 RenderPipeline* pipeline = nullptr;
 
-                if (kid->renderID != INT_MAX) {
-                    pipeline = pipelines.at(kid->renderID);
-
-                    pipeline->VertexShader->Release();
-                    pipeline->PixelShader->Release();
-                    pipeline->VertexBuffer->Release();
-                    pipeline->IndexBuffer->Release();
-
-                    pipeline->VertexShader = nullptr;
-                    pipeline->PixelShader = nullptr;
-                    pipeline->VertexBuffer = nullptr;
-                    pipeline->IndexBuffer = nullptr;
-                }
-                else {
-                    pipeline = new RenderPipeline;
-                }
-
-                MeshData partMesh;
-                gen.GenerateCube(p->Size, &partMesh);
-
-                pipeline->ViewMatrix = DirectX::XMMatrixTranslation(p->Position.X, p->Position.Y, p->Position.Z);
-
-                D3D11_BUFFER_DESC vertexBufferDesc;
-                ZeroMemory(&vertexBufferDesc, sizeof(D3D11_BUFFER_DESC));
-                vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-                vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-                vertexBufferDesc.ByteWidth = sizeof(MeshData::vertex) * partMesh.vertexMap.size();
-                vertexBufferDesc.CPUAccessFlags = 0;
-
-                D3D11_SUBRESOURCE_DATA resourceDataVertex;
-                ZeroMemory(&resourceDataVertex, sizeof(D3D11_SUBRESOURCE_DATA));
-                resourceDataVertex.pSysMem = &(partMesh.vertexMap.front());
-
-                pdx_Device->CreateBuffer(&vertexBufferDesc, &resourceDataVertex, &pipeline->VertexBuffer);
-
-                D3D11_BUFFER_DESC indexBufferDesc;
-                ZeroMemory(&indexBufferDesc, sizeof(D3D11_BUFFER_DESC));
-                indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
-                indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-                indexBufferDesc.ByteWidth = sizeof(unsigned int) * partMesh.vertexIndices.size() * 3;
-                indexBufferDesc.CPUAccessFlags = 0;
-
-                D3D11_SUBRESOURCE_DATA irinitData;
-                irinitData.pSysMem = &(partMesh.vertexIndices.front());;
-                pdx_Device->CreateBuffer(&indexBufferDesc, &irinitData, &pipeline->IndexBuffer);
-
-                LoadShader(ST_VertexShader, pipeline, "data/shaders/rshader_commonobj.shader");
-                LoadShader(ST_PixelShader, pipeline, "data/shaders/rshader_commonobj.shader");
-
-                pipeline->VertexCount = partMesh.vertexMap.size();
-                pipeline->IndexCount = partMesh.vertexIndices.size();
-
                 if (kid->renderID == INT_MAX) {
+                    pipeline = new RenderPipeline;
+
+                    MeshData partMesh;
+                    gen.GenerateCube(Vector3(1, 1, 1), &partMesh);
+
+                    D3D11_BUFFER_DESC vertexBufferDesc;
+                    ZeroMemory(&vertexBufferDesc, sizeof(D3D11_BUFFER_DESC));
+                    vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+                    vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+                    vertexBufferDesc.ByteWidth = sizeof(MeshData::vertex) * partMesh.vertexMap.size();
+                    vertexBufferDesc.CPUAccessFlags = 0;
+
+                    D3D11_SUBRESOURCE_DATA resourceDataVertex;
+                    ZeroMemory(&resourceDataVertex, sizeof(D3D11_SUBRESOURCE_DATA));
+                    resourceDataVertex.pSysMem = &(partMesh.vertexMap.front());
+
+                    pdx_Device->CreateBuffer(&vertexBufferDesc, &resourceDataVertex, &pipeline->VertexBuffer);
+
+                    D3D11_BUFFER_DESC indexBufferDesc;
+                    ZeroMemory(&indexBufferDesc, sizeof(D3D11_BUFFER_DESC));
+                    indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+                    indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+                    indexBufferDesc.ByteWidth = sizeof(unsigned int) * partMesh.vertexIndices.size() * 3;
+                    indexBufferDesc.CPUAccessFlags = 0;
+
+                    D3D11_SUBRESOURCE_DATA irinitData;
+                    irinitData.pSysMem = &(partMesh.vertexIndices.front());;
+                    pdx_Device->CreateBuffer(&indexBufferDesc, &irinitData, &pipeline->IndexBuffer);
+
+                    LoadShader(ST_VertexShader, pipeline, "data/shaders/rshader_commonobj.shader");
+                    LoadShader(ST_PixelShader, pipeline, "data/shaders/rshader_commonobj.shader");
+
+                    pipeline->VertexCount = partMesh.vertexMap.size();
+                    pipeline->IndexCount = partMesh.vertexIndices.size();
+
+                    pipeline->ViewMatrix = DirectX::XMMatrixScaling(p->Size.X, p->Size.Y, p->Size.Z);
+                    pipeline->ViewMatrix *= DirectX::XMMatrixTranslation(p->Position.X, p->Position.Y, p->Position.Z);
+
                     kid->renderID = pipelines.size();
                     pipelines.push_back(pipeline);
                 }
+                else {
+                    pipeline = pipelines.at(kid->renderID);
+
+                    pipeline->ViewMatrix = DirectX::XMMatrixScaling(p->Size.X, p->Size.Y, p->Size.Z);
+                    pipeline->ViewMatrix *= DirectX::XMMatrixTranslation(p->Position.X, p->Position.Y, p->Position.Z);
+                }
+
             }
             if (kid->ClassName == "Material") {
 
